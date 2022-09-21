e441957a45badd1cfc32a22b2af93d9b972e354f
florencialarreategui <florencialarreategui@gmail.com>
2022-09-20 22:59:37 -0300

    agrego botones

--- screens/start-game.js
+++ screens/start-game.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Button } from "react-native";
 
 const style = StyleSheet.create({
     container:{
@@ -10,4 +10,27 @@ const style = StyleSheet.create({
     }
 })
 
-const StartGameScreen = () =>
+const StartGameScreen = () =>{
+    return(
+        <View style={StyleSheet.container}>
+            <Text style={styles.title}>Comenzar juego</Text>
+            <View style={styles.inputContainer}>
+                <Text style={styles.label}>Elija un número</Text>
+                <TextInput/>
+                <View style={styles.buttonContainer}>
+                    <Button
+                    title="Confirmar"
+                    onPress={( () => null)}
+                    color= "#392F5A"
+                    />
+                     <Button
+                    title="Confirmar"
+                    onPress={( () => null)}
+                    color= "#392F5A"
+                    />
+                </View>
+            </View>
+        </View>
+    )
+}
+export default StartGameScreen;
