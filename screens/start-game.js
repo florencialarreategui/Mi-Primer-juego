8c8d64546519d8d4bf7387bb96d3f22c3fcac273
florencialarreategui <florencialarreategui@gmail.com>
2022-09-21 23:07:49 -0300

    arreglo de errores

--- screens/start-game.js
+++ screens/start-game.js
@@ -41,7 +41,7 @@ const styles = StyleSheet.create({
         with:"75%",
         marginHorizontal: 20,
         flexDirection: "row",
-        justifyContent:" space-around",
+        justifyContent:"space-around",
         marginTop: 20,
     }
 
